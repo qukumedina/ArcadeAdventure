3a5d2e21542cce5be82787c6744976c331286543
qukujasmina <ic22b088@technikum-wien.at>
2024-01-10 22:12:11 +0100

    Add class for Player

---
+++ src/main/java/arcadeAdventures/TicTacToe/Player.java
@@ -0,0 +1,13 @@
+package arcadeAdventures.TicTacToe;
+
+public class Player {
+    private char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return this.marker;
+    }
+}
