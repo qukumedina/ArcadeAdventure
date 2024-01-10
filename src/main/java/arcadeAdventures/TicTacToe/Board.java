18a0927165f555b8ce75a406d57e65bfa33d20db
qukujasmina <ic22b088@technikum-wien.at>
2024-01-10 22:11:05 +0100

    Add class for Board

---
+++ src/main/java/arcadeAdventures/TicTacToe/Board.java
@@ -0,0 +1,37 @@
+package arcadeAdventures.TicTacToe;
+
+public class Board {
+    private char[][] cells = new char[3][3];
+
+    public Board() {
+        //TODO
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        //TODO
+        return false;
+    }
+
+    public void place(int x, int y, char marker) {
+        //TODO
+    }
+
+    public boolean isFull() {
+        //TODO
+        return false;
+    }
+
+    public void clear() {
+        //TODO
+    }
+
+
+    public void print() {
+        //TODO
+    }
+
+    public char checkWinner() {
+        //TODO
+        return '0';
+    }
+}
